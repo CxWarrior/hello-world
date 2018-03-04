833fc9bd7ec20eb52851128ddbe75ba73f3aa161
PaulJulio <paulhulett@gmail.com>
2018-03-04 15:52:53 -0800

    scoring and level progression

--- game/game.js
+++ game/game.js
@@ -1,4 +1,5 @@
-var world;
+var world, balls = [], pegs = [], maxBalls = 1, score = 0, lives = 30, level = 1;
+var debugContact = null;
 
 function init() {
     var b2Vec2 = Box2D.Common.Math.b2Vec2
@@ -11,6 +12,7 @@ function init() {
     ,	b2PolygonShape = Box2D.Collision.Shapes.b2PolygonShape
     ,	b2CircleShape = Box2D.Collision.Shapes.b2CircleShape
     ,	b2DebugDraw = Box2D.Dynamics.b2DebugDraw
+    ,   groundBody, pegBody, otherBody
     ;
 
     world = new b2World(
@@ -31,80 +33,31 @@ function init() {
     bodyDef.position.y = 33;
     fixDef.shape = new b2PolygonShape;
     fixDef.shape.SetAsBox(17, 0.5);
-    world.CreateBody(bodyDef).CreateFixture(fixDef);
+    groundBody = world.CreateBody(bodyDef);
+    groundBody.CreateFixture(fixDef);
+    groundBody.SetUserData({'ground':true});
 
     // top wall
     bodyDef.position.y = 0.5;
-    world.CreateBody(bodyDef).CreateFixture(fixDef);
+    otherBody = world.CreateBody(bodyDef);
+    otherBody.CreateFixture(fixDef);
+    otherBody.SetUserData({'wall':true});
 
     // left wall
     bodyDef.position.x = 0.5;
     bodyDef.position.y = 17;
     fixDef.shape.SetAsBox(0.5, 17);
-    world.CreateBody(bodyDef).CreateFixture(fixDef);
+    otherBody = world.CreateBody(bodyDef);
+    otherBody.CreateFixture(fixDef);
+    otherBody.SetUserData({'wall':true});
 
     // right wall
     bodyDef.position.x = 33;
-    world.CreateBody(bodyDef).CreateFixture(fixDef);
+    otherBody = world.CreateBody(bodyDef);
+    otherBody.CreateFixture(fixDef);
+    otherBody.SetUserData({'wall':true});
 
-    // peg definition
-    fixDef.shape = new b2CircleShape(0.2);
-    // peg locations level 1
-    /*
-    var pegs = [
-        {x:4, y:22}
-        ,{x:6, y:11}
-        ,{x:8, y:16}
-        ,{x:10, y:21.25}
-        ,{x:12, y:10.5}
-        ,{x:14, y:7}
-        ,{x:16, y:10.5}
-        ,{x:18, y:21.25}
-        ,{x:20, y:16}
-        ,{x:22, y:11}
-        ,{x:24, y:22}
-    ];
-    /* end level 1 */
-    // peg locations level 2
-    /*
-    var pegs = [
-        {x:6.5, y:25}
-        ,{x:6.5, y:17}
-        ,{x:9, y:19.75}
-        ,{x:6.5, y:13}
-        ,{x:10, y:10}
-        ,{x:12, y:15}
-        ,{x:14, y:10}
-        ,{x:16.5, y:13}
-        ,{x:15, y:19.75}
-        ,{x:16.5, y:17}
-        ,{x:18.5, y:25}
-    ];
-    /* end level 2*/
-    /* */
-    var pegs = [
-        {x:7, y:22.5}
-        ,{x:6, y:18.5}
-        ,{x:7.5, y:15}
-        ,{x:9, y:12}
-        ,{x:11, y:9}
-        ,{x:12, y:6}
-        ,{x:13, y:9}
-        ,{x:15, y:12}
-        ,{x:16.75, y:15}
-        ,{x:18, y:18.5}
-        ,{x:17, y:22.5}
-        ,{x:12, y:26}
-        ,{x:15, y:25}
-        ,{x:9, y:25}
-    ];
-
-    // draw current level on the board
-    for (var i = 0; i < pegs.length; i++) {
-        bodyDef.position.x = pegs[i].x;
-        bodyDef.position.y = pegs[i].y;
-        world.CreateBody(bodyDef).CreateFixture(fixDef);
-    }
+    drawLevel();
 
     //setup debug draw
     var debugDraw = new b2DebugDraw();
@@ -119,17 +72,84 @@ function init() {
 };
 
 function update() {
+    var i, ball, contact, peg;
+    /*
+    if (debugContact !== null) {
+        return;
+    }
+    */
     world.Step(
         1 / 60   //frame-rate
         ,  10       //velocity iterations
         ,  10       //position iterations
     );
+    // start game logic
+    for (i in balls) {
+        ball = balls[i];
+        contact = ball.GetBody().GetContactList();
+        if (contact === null) {
+            continue;
+        }
+        debugContact = contact;
+        if (contact.other.GetUserData().ground) {
+            ball.GetBody().SetActive(false);
+            world.DestroyBody(ball.GetBody());
+        }
+        if (contact.other.GetUserData().peg) {
+            contact.other.GetUserData().hit = true;
+        }
+    }
+    // check to see if there is at least one ball still active
+    var active = false;
+    for (i in balls) {
+       if (active === true) {
+           continue;
+       }
+       active = balls[i].GetBody().IsActive() && balls[i].GetBody().IsAwake();
+    }
+    if (active === false && balls.length) {
+        // if no more balls are active, do this logic
+        // remove hit pegs
+        activePegs = 0;
+        for (i in pegs) {
+            peg = pegs[i];
+            if (peg.GetBody().IsActive() && peg.GetBody().GetUserData().hit === true) {
+                peg.GetBody().SetActive(false);
+                world.DestroyBody(peg.GetBody());
+                score += 1;
+            } else if (peg.GetBody().IsActive()) {
+                activePegs += 1;
+            }
+        }
+        // reset ball list
+        balls = [];
+        if (activePegs === 0) {
+            level += 1;
+            drawLevel();
+        } else {
+            lives -= 1;
+        }
+        // update the score
+        updatePage();
+    }
+    // end game logic
     world.DrawDebugData();
     world.ClearForces();
-};
+}
+
+function updatePage() {
+    $('#score').html(score);
+    $('#lives').html(lives);
+}
 
 function dropball(e){
-    var fixDef = new Box2D.Dynamics.b2FixtureDef;
+    if (balls.length >= maxBalls) {
+        return;
+    }
+    if (lives < 1) {
+        return;
+    }
+    var fixDef = new Box2D.Dynamics.b2FixtureDef, ball;
     fixDef.density = 1.0;
     fixDef.friction = 0.5;
     fixDef.restitution = 0.9;
@@ -137,13 +157,114 @@ function dropball(e){
     var bodyDef = new Box2D.Dynamics.b2BodyDef;
     bodyDef.type = Box2D.Dynamics.b2Body.b2_dynamicBody;
     bodyDef.position.x = e.offsetX * 30/1000 + 1.25;
-    bodyDef.position.y = 1;
-    world.CreateBody(bodyDef).CreateFixture(fixDef);
+    bodyDef.position.y = 1.5;
+    ball = world.CreateBody(bodyDef);
+    balls.push(ball.CreateFixture(fixDef));
+    ball.SetUserData({'ball':true});
+}
+
+function restartLevel() {
+    if (balls.length > 0) {
+        return;
+    }
+    for (var i in pegs) {
+        var peg = pegs[i];
+        world.DestroyBody(peg.GetBody());
+    }
+    score = 0;
+    lives = 3;
+    balls = [];
+    pegs = [];
+    drawLevel();
+    updatePage();
+}
+
+function getPegsForCurrentLevel() {
+
+    // peg locations level 1
+    if (level === 1) {
+        return [{x:4, y:22}
+            ,{x:6, y:11}
+            ,{x:8, y:16}
+            ,{x:10, y:21.25}
+            ,{x:12, y:10.5}
+            ,{x:14, y:7}
+            ,{x:16, y:10.5}
+            ,{x:18, y:21.25}
+            ,{x:20, y:16}
+            ,{x:22, y:11}
+            ,{x:24, y:22}
+        ];
+    }
+    // peg locations level 2
+    if (level === 2) {
+        return [{x:6.5, y:25}
+            ,{x:6.5, y:17}
+            ,{x:9, y:19.75}
+            ,{x:6.5, y:13}
+            ,{x:10, y:10}
+            ,{x:12, y:15}
+            ,{x:14, y:10}
+            ,{x:16.5, y:13}
+            ,{x:15, y:19.75}
+            ,{x:16.5, y:17}
+            ,{x:18.5, y:25}
+        ];
+    }
+    return [{x:7, y:22.5}
+        ,{x:6, y:18.5}
+        ,{x:7.5, y:15}
+        ,{x:9, y:12}
+        ,{x:11, y:9}
+        ,{x:12, y:6}
+        ,{x:13, y:9}
+        ,{x:15, y:12}
+        ,{x:16.75, y:15}
+        ,{x:18, y:18.5}
+        ,{x:17, y:22.5}
+        ,{x:12, y:26}
+        ,{x:15, y:25}
+        ,{x:9, y:25}
+    ];
+}
+
+function drawLevel() {
+    var 	b2BodyDef = Box2D.Dynamics.b2BodyDef
+        ,	b2Body = Box2D.Dynamics.b2Body
+        ,	b2FixtureDef = Box2D.Dynamics.b2FixtureDef
+        ,	b2CircleShape = Box2D.Collision.Shapes.b2CircleShape
+    ;
+
+    var fixDef = new b2FixtureDef;
+    fixDef.density = 1.0;
+    fixDef.friction = 0.5;
+    fixDef.restitution = 0.9;
+
+    var bodyDef = new b2BodyDef;
+
+    //create ground
+    bodyDef.type = b2Body.b2_staticBody;
+    // peg definition
+    fixDef.shape = new b2CircleShape(0.4); // size of peg
+
+    pegCoords = getPegsForCurrentLevel();
+
+    // draw current level on the board
+    for (var i = 0; i < pegCoords.length; i++) {
+        bodyDef.position.x = pegCoords[i].x;
+        bodyDef.position.y = pegCoords[i].y;
+        pegBody = world.CreateBody(bodyDef);
+        pegs.push(pegBody.CreateFixture(fixDef));
+        pegBody.SetUserData({'peg':true, 'hit':false});
+    }
 }
 
 $(function(){
     // on ready
     $('#canvas').click(function(e){
         dropball(e);
+    });
+    $('#restart').click(function(e) {
+        restartLevel();
     });
 });
